d28689f25e1e51a1dcdc7f1e323b36c1e99d406e
Ricardo Dias <ricardo.j.dias@pgr.pt>
2022-08-05 16:59:35 +0100

    Markup Editor in Vue component and form with post to create new Request

--- resources/js/Components/CreateRequest.vue
+++ resources/js/Components/CreateRequest.vue
@@ -1,25 +1,36 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import { useForm } from '@inertiajs/inertia-vue3';
+import { ref, onMounted, onUpdated } from 'vue'
+import { useForm, usePage } from '@inertiajs/inertia-vue3';
+import Editor from '@toast-ui/editor'
+import '@toast-ui/editor/dist/toastui-editor.css'
 
 import Modal from '@/Components/Modal.vue'
+
+const props = defineProps({
+    store_url: String
+})
+
 
 let showModal = ref(false)
 
 let error = ref('')
-let app = ref('')
 let apps = ref([])
 
 const form = useForm({
-    pedido: '',
+    title: '',
     app: '',
-    description: ''
+    description: '',
+    requested_by: ''
 })
+
+const editorHtml = ref(null)
+let editor = null
+
 
 onMounted(async () => {
     try {
         const res = await fetch('/apps/short')
-        if (!res.ok) error.value = "Erro ao obter aplicatioções!"
+        if (!res.ok) error.value = "Erro ao obter lista de aplicatioções!"
         else
             apps.value = await res.json()
     } catch (err) {
@@ -28,6 +39,33 @@ onMounted(async () => {
     }
 })
 
+onUpdated(() => {
+    if (editorHtml.value) {
+        editor = new Editor({
+            el: editorHtml.value,
+            height: '400px',
+            initialEditType: 'markdown',
+            placeholder: 'Write something cool!',
+        })
+        editor.setMarkdown(form.description)
+    }
+})
+
+const createRequest = () => {
+    console.log(editor.getMarkdown())
+    form.post(props.store_url)
+}
+
+const getMarkdown = (e) => {
+    console.log(editor.getMarkdown())
+    form.description = editor.getMarkdown()
+}
+
+function closeModal() {
+    console.log("Oi!!");
+    form.description = editor.getMarkdown()
+    showModal.value = false
+}
 
 </script>
 
@@ -36,23 +74,37 @@ onMounted(async () => {
         pedido...
     </button>
     <Teleport to="body">
-        <Modal v-if="showModal" @close="showModal = false">
+        <Modal v-if="showModal" @close="closeModal">
             <template #header>
                 <h2 class="font-bold text-lg mb-3">Criar pedido</h2>
             </template>
             <template #default>
-                <div class="flex flex-col gap-2">
-                    <input class="px-2 py-1 flex-1 border-red-400 border rounded"
-                        placeholder="Titulo do novo pedido..." v-model="form.pedido">
-                    <select v-model="form.app" class="px-2 py-1 border-red-400 rounded">
-                        <option value="" disabled>Selectione uma aplicação</option>
-                        <option v-for="application in apps" :key="application.id">{{ application }}
-                        </option>
-                    </select>
-                    <textarea cols="30" rows="10" class="px-2 py-2 border-red-400 rounded"
-                        v-model="form.description"></textarea>
-                </div>
-                <p>{{ error }}</p>
+                <form @submit.prevent="createRequest">
+                    <div class="flex flex-col gap-2">
+                        <input class="px-2 py-1 flex-1 border-red-400 border rounded"
+                            placeholder="Titulo do novo pedido..." v-model="form.title">
+                        <select v-model="form.app" class="px-2 py-1 border-red-400 rounded">
+                            <option value="" disabled>Selectione uma aplicação</option>
+                            <option v-for="application in apps" :key="application.id">{{ application
+                            }}
+                            </option>
+                        </select>
+                        <!-- <textarea cols="30" rows="10" class="px-2 py-2 border-red-400 rounded"
+                            v-model="form.description"></textarea> -->
+                        <div>
+                            <label for="editor">Descrição:</label>
+                            <div id="editor" ref="editorHtml" @keydown="getMarkdown"></div>
+                        </div>
+                        <label for="editor">Pedido por:</label>
+                        <input class="px-2 py-1 flex-1 border-red-400 border rounded"
+                            v-model="form.requested_by" placeholder="Maria João">
+                    </div>
+
+
+                    <p v-if="error">{{ error }}</p>
+                    <button class="mt-3 px-2 py-1 bg-red-500
+                        border-red-400 text-white rounded-xl">Ok</button>
+                </form>
             </template>
         </Modal>
     </Teleport>
--- resources/js/Components/Modal.vue
+++ resources/js/Components/Modal.vue
@@ -4,19 +4,21 @@
 
 <template>
     <div class="fixed inset-0 bg-gray-600 opacity-80 grid place-items-center">
-        <div class="bg-white text-black w-4/5 max-w-2xl p-6 rounded-md opacity-95">
+        <div class="bg-white text-black w-4/5 max-w-2xl p-6 rounded-md opacity-100">
             <header>
-                <slot name="header" v-if="$slots.header">
-                </slot>
+                <div class="flex justify-between items-start">
+                    <slot name="header" v-if="$slots.header">
+                    </slot>
+                    <button class="pr-3 font-bold text-3xl" @click="$emit('close')">&times;</button>
+                </div>
             </header>
             <div>
                 <slot name="default">
                 </slot>
             </div>
             <footer>
-                <slot name=footer>
-                    <button class="mt-3 px-2 py-1 bg-red-500 border-red-400 text-white
-                        rounded-xl" @click="$emit('close')">Close</button>
+                <slot name="footer">
+
                 </slot>
             </footer>
 
